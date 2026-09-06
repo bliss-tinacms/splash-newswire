a2a72348e10a883937ed7e2fe359c28f18adecf2
joosh-bd <joshua@blissdrive.net>
2026-09-07 02:13:14 +0800

    Update Tina schema lock file

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: 'C:/Users/ADMIN/Desktop/Astro/tina-blog/tina/__generated__/.cache/1788715114461', url: 'https://www.splashnewswire.com/tina-content-proxy', token: '23e61af1d530b05eb4287b6b78bfe3ab28935648', queries,  });
+export const client = createClient({ url: 'https://www.splashnewswire.com/tina-content-proxy', token: '23e61af1d530b05eb4287b6b78bfe3ab28935648', queries,  });
 export default client;
   
